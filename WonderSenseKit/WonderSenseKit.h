10a27bcd7847275a53050485b2e42c45329457fc
0eta0 <eta.develop@gmail.com>
2020-07-24 11:29:53 +0900

    🚀

---
+++ WonderSenseKit/WonderSenseKit.h
@@ -0,0 +1,19 @@
+//
+//  WonderSenseKit.h
+//  WonderSenseKit
+//
+//  Created by Eita Yamaguchi on 2020/07/24.
+//  Copyright © 2020 WonderWonder. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WonderSenseKit.
+FOUNDATION_EXPORT double WonderSenseKitVersionNumber;
+
+//! Project version string for WonderSenseKit.
+FOUNDATION_EXPORT const unsigned char WonderSenseKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WonderSenseKit/PublicHeader.h>
+
+
